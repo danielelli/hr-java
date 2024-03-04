19ac9837ab1c305ebac5299072d3c80081da2a7d
tocipher <dan.elli@outlook.com>
2024-03-03 23:56:35 -0600

    Solve welcome-to-java problem

---
+++ easy/welcome-to-java.java
@@ -0,0 +1,11 @@
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Print "Hello, World." on the first line
+        System.out.println("Hello, World.");
+
+        // Print "Hello, Java." on the second line
+        System.out.println("Hello, Java.");
+    }
+}
