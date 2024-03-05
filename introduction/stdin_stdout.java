bdd8ee174cbcd5af7a111f1edae8532dcce73e41
tocipher <dan.elli@outlook.com>
2024-03-04 20:01:17 -0600

    Solve stdin_stdout problem

---
+++ introduction/stdin_stdout.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution 
+{
+    public static void main(String[] args)
+    {
+        // Create Scanner
+        Scanner scnr = new Scanner(System.in);
+        // Read next 3 integer input
+        int a = scnr.nextInt();
+        int b = scnr.nextInt();
+        int c = scnr.nextInt();
+        // Close Scanner
+        scnr.close();
+        //Print the variables values
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
